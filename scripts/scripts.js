0f64b58739c3f191804c7781ab2658a72327b065
Ben Margevicius <bdm4@case.edu>
2014-08-11 14:25:30 -0400

    Updated Scripts. Removed old web.config.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,70 +1,61 @@
+$(function(){	
+	//if scrolling past 100px down fade in the button that allows the user to scroll back up
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('#back-top').fadeIn();
+		} else {
+			$('#back-top').fadeOut();
+		}
+	});
 	
-// Back to top
-	jQuery(document).ready(function () {
-		$(window).scroll(function () {
-			if (jQuery(this).scrollTop() > 100) {
-				jQuery('#back-top').fadeIn();
-			} else {
-				jQuery('#back-top').fadeOut();
-			}
-		});
+	//this is the button that fades in past 100px, it allows the screen to scroll back up to the top of the page.
+	$('#back-top').click(function (e) {
+		e.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0
+		}, 300);
+		//return false;
+	});
+	
+	// Global links toggle
+	$('#global-menu-toggle').click(function(e) {
+		e.preventDefault();
+		$('#global-menu').slideToggle('fast');
+		$(this).toggleClass('active');
+		//return false;
+	});
+	//mobile nav
+	$('.mobile-nav .dropmenu li a').click(function(e) {
+		e.preventDefault();
+		var openMe = $(this).next();
+		var mySiblings = $(this).parent().siblings().find('ul');
+		if (openMe.is(':visible')) {
+			openMe.slideUp('normal');  
+		} else {
+			mySiblings.slideUp('normal');  
+			openMe.slideDown('normal');
+		}
+	});
+	
+	// Mobile Nav
+	$('.mobile-nav-toggle, .mobile-nav-toggle_smartphone').click(function (e) {
+		e.preventDefault();
+		$('.mobile-nav .dropmenu').slideToggle('fast');
+	});
+});
 
-		jQuery('#back-top').click(function () {
-			jQuery('body,html').animate({
-				scrollTop: 0
-			}, 300);
-			return false;
-		});
-	}); 
-
-
-// Slider
 $(window).load(function() {
-				$('.flexslider').flexslider({
-				  animation: "slide",
-				  controlsContainer: ".flex-container"
-			  });
-			});
- 
-// Mobile Nav
-jQuery(document).ready(function() {
-	jQuery('.mobile-nav .dropmenu li a').click(
-		function() {
-			var openMe = $(this).next();
-			var mySiblings = $(this).parent().siblings().find('ul');
-			if (openMe.is(':visible')) {
-				openMe.slideUp('normal');  
-			} else {
-				mySiblings.slideUp('normal');  
-				openMe.slideDown('normal');
-			}
-	      }
-	);
+	$('.flexslider').flexslider({
+		animation: "slide",
+		controlsContainer: ".flex-container"
+	});
 	
-	jQuery('.mobile-nav-toggle, .mobile-nav-toggle_smartphone').click (function () {
-	jQuery('.mobile-nav .dropmenu ').slideToggle('fast');
-	return false;
-		
-	});
-
+	$(".desktop-nav").sticky({ topSpacing: -5 });
 });
-
-// Global links toggle
-jQuery(document).ready(function() {
-	jQuery('#global-menu-toggle').click(function() {
-	jQuery('#global-menu').slideToggle('fast');
-	jQuery(this).toggleClass('active');
-	return false;
-		
-	});
-		
-});
+ 
 
 
-// Sticky nav
-    jQuery(window).load(function(){
-      jQuery(".desktop-nav").sticky({ topSpacing: -5 });
-    });
+
 
 
 
